54a901e06e6410bba647daf0d6820c4dd09ab258
anu-pradeep <anupriyapradeepanus@gmail.com>
2024-06-26 14:47:40 +0530

    hai

--- lib/all_charts_is_here/spark area chart.dart
+++ lib/all_charts_is_here/spark area chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_charts/all_charts_is_here/CustomAPPBAR.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 
@@ -9,20 +10,21 @@ class SparkareaChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Spark Area Chart'),
-        ),
+        appBar: const Customappbar(apptext: "Spark area Chart"),
         body: Center(
-          child: SfSparkAreaChart(
-            color: Colors.grey,
-            trackball: const SparkChartTrackball(
-              activationMode: SparkChartActivationMode.tap,
+          child: SizedBox(
+            height: 150,
+            child: SfSparkAreaChart(
+              color: Colors.grey,
+              trackball: const SparkChartTrackball(
+                activationMode: SparkChartActivationMode.tap,
+              ),
+              marker: const SparkChartMarker(
+                displayMode: SparkChartMarkerDisplayMode.all,
+              ),
+              // labelDisplayMode: SparkChartLabelDisplayMode.all,
+              data: const <double>[1, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10],
             ),
-            marker: const SparkChartMarker(
-              displayMode: SparkChartMarkerDisplayMode.all,
-            ),
-            // labelDisplayMode: SparkChartLabelDisplayMode.all,
-            data: const <double>[1, 5, 2, 6, 3, 7, 4, 8, 5, 9, 6, 10],
           ),
         ),
 
